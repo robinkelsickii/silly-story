7fe3159f2d663b67cbeb827f8cbf9a952e42d5c0
robinkelsickii <robinkelsickii@gmail.com>
2021-09-09 00:06:20 -0400

    started the silly story project. Left off on newStory variable.

---
+++ main.js
@@ -0,0 +1,43 @@
+
+const customName = document.getElementById('customname');
+const randomize = document.querySelector('.randomize');
+const story = document.querySelector('.story');
+
+function randomValueFromArray(array){
+  const random = Math.floor(Math.random()*array.length);
+  return array[random];
+}
+
+var storyText = "It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.";
+
+const insertX = ['Willy the Goblin', 'Big Daddy', 'Father Christmas'];
+const insertY =['the soup kitchen', 'Disneyland', 'The White House'];
+const insertZ = ['spontaneously combusted', 'melted into a puddle on the sidewalk', 'turned into a slug and crawled away'];
+
+let xItem = randomValueFromArray(insertX);
+let yItem = randomValueFromArray(insertY);
+let zItem = randomValueFromArray(insertZ);
+
+var newStory = storyText;
+newStory = newStory.replace(':inserty:', xItem);
+newStory = newStory.replace(':insertz:',zItem);
+newStory = newStory.replace(':insertx:',xItem).replace(':insertx:',xItem);
+
+randomize.addEventListener('click', result);
+
+function result() {
+
+  if(customName.value !== '') {
+    let name = customName.value;
+
+  }
+
+  if(document.getElementById("uk").checked) {
+    let weight = Math.round(300);
+    let temperature =  Math.round(94);
+
+  }
+
+  story.textContent = ;
+  story.style.visibility = 'visible';
+}
